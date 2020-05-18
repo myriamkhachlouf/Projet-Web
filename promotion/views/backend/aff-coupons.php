f67199ce17b9809130c952a520be292f5fef56db
salahdabbech1 <58320947+salahdabbech1@users.noreply.github.com>
2020-05-19 00:00:56 +0100

    changer affichage pour trie

--- promotion/views/backend/aff-coupons.php
+++ promotion/views/backend/aff-coupons.php
@@ -265,19 +265,21 @@ $listcoup=$coup1c->afficher_coup();
     <section class="content">
       <div class="row">
         <div class="col-12">
-         <div class="card">
+          <div class="card">
             <div class="card-header">
-              <h3 class="card-title">DataTable </h3>
+              <h3 class="card-title">DataTable with default features</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-              <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6"><div class="dataTables_length" id="example1_length"><label>Show <select name="example1_length" aria-controls="example1" class="custom-select custom-select-sm form-control form-control-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-12 col-md-6"><div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="example1" class="table table-bordered table-striped dataTable dtr-inline" role="grid" aria-describedby="example1_info">
+              <table id="example1" class="table table-bordered table-striped">
                 <thead>
-                <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="ID Solde: activate to sort column descending">Id coupons</th>
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Prix Reduction: activate to sort column ascending">Prix Reduction</th>
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Id Produit: activate to sort column ascending">Id Client</th>
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Date debut: activate to sort column ascending">Date debut</th>
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Date fin: activate to sort column ascending">Date fin</th></tr>
+                <tr>
+                  <th>Rendering engine</th>
+                  <th>Browser</th>
+                  <th>Platform(s)</th>
+                  <th>Engine version</th>
+                  <th>CSS grade</th>
+                </tr>
                 </thead>
                 <tbody>
                 <?php
@@ -295,14 +297,23 @@ $listcoup=$coup1c->afficher_coup();
                 ?>
                 </tbody>
                 <tfoot>
-                <tr><th rowspan="1" colspan="1">Id coupons</th><th rowspan="1" colspan="1">Prix reduction</th><th rowspan="1" colspan="1">Id Client</th><th rowspan="1" colspan="1">Date debut</th><th rowspan="1" colspan="1">Date fin</th></tr>
+                <tr>
+                  <th>Rendering engine</th>
+                  <th>Browser</th>
+                  <th>Platform(s)</th>
+                  <th>Engine version</th>
+                  <th>CSS grade</th>
+                </tr>
                 </tfoot>
-              </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li><li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
+              </table>
             </div>
             <!-- /.card-body -->
           </div>
+          <!-- /.card -->
         </div>
-      </div>  
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
     </section>
   </div>
 
