4144a24244439cc642405816fc1db2521745e62a
mohamedarbi1 <47950542+mohamedarbi1@users.noreply.github.com>
2020-06-09 19:11:29 +0200

    Add files via upload

---
+++ config2.php
@@ -0,0 +1,6 @@
+<?php
+$con=mysqli_connect('localhost','root','','tmart');
+if(mysqli_connect_errno())
+{
+	echo 'Failed'.mysqli_connect_error();
+}
---
+++ pdf.php
@@ -0,0 +1,70 @@
+<?php
+include "C:\wamp64\www\arbi projet\back-end\config2.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Print</title>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2>ScreenShop</h2>
+      <table class="table table-bordered print">
+        <thead>
+          <tr>
+            <th>S.No</th>
+            <th>Reference</th>
+            <th>Id </th>
+            <th>Nom </th>
+            <th>Prenom </th>
+            <th>Marque </th>
+            <th>Produit</th>
+            <th>Telephone</th>
+            <th>Email</th>
+            <th>date de création</th>
+            <th>Quantité</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $sn=1;
+          $user_qry="SELECT * from vendeur";
+          $user_res=mysqli_query($con,$user_qry);
+          while($user_data=mysqli_fetch_assoc($user_res))
+          {
+          ?>
+          <tr>
+            <td><?php echo $sn; ?></td>
+            <td><?php echo $sn; ?></td>
+            <td><?php echo $user_data["id"]; ?></td>
+            <td><?php echo $user_data["nom"]; ?></td>
+            <td><?php echo $user_data["prenom"]; ?></td>
+            <td><?php echo $user_data['marque']; ?></td>
+            <td><?php echo $user_data['produit']; ?></td>
+            <td><?php echo $user_data['telephone']; ?></td>
+            <td><?php echo $user_data['email']; ?></td>
+            <td><?php echo $user_data['quantite']; ?></td>
+            <td><?php echo $user_data['date_crea']; ?></td>
+            
+          </tr>
+          <?php
+          $sn++;
+          }
+          ?>
+        </tbody>
+      </table>
+
+      <div class="text-center">
+        <a href="print.php" class="btn btn-primary">Print</a>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
